f38cf033af56210bb1abe6d8a5e6cb8f55bcb2f9
dt_love_gj <2601261664@qq.com>
2020-11-12 23:39:07 +0800

    初始化仓库

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  tabs: false,
+  semi: false,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  tailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+  // 去除尾随逗号
+  trailingComma: 'none'
+}
